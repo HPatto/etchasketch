48bf85a51054c51f186027684f794e73d94216d1
HPatto <hpatto97@gmail.com>
2023-12-19 20:27:26 -0300

    Develop basic canvas grid

--- etchasketch.js
+++ etchasketch.js
@@ -2,3 +2,49 @@
 A Javascript file, controlling the functionality and apperance
 of the etch-a-sketch webpage
 */
+
+// Build the canvas grid
+function buildCanvasGrid(sideCount) {
+    // Build a div container element
+    const divContainer = document.createElement('div');
+
+    // Give it a CSS property that has flexbox defined.
+    divContainer.classList.add("canvas-container");
+
+    for (let i = 0; i < sideCount; i++) {
+        let divRow = buildCanvasRow(sideCount);
+        divContainer.appendChild(divRow);
+    }
+
+    return divContainer;
+
+}
+
+// Construct a row of the canvas grid
+function buildCanvasRow(sideCount) {
+    // Build a div element
+    const divRow = document.createElement('div');
+
+    // Give it a CSS property that has flexbox defined.
+    divRow.classList.add("canvas-row");
+
+    for (let i = 0; i < sideCount; i++) {
+        const divElem = document.createElement('div');
+        divElem.classList.add("canvas-element");
+        // console.log("Hey");
+        divRow.appendChild(divElem);
+    }
+
+    return divRow;
+
+}
+
+// Insert the canvas grid into the document
+function insertCanvasGrid(newGrid) {
+    const canvasAnchor = document.querySelector(".canvas");
+    canvasAnchor.appendChild(newGrid);
+}
+
+addEventListener('DOMContentLoaded', function () {
+    insertCanvasGrid(buildCanvasGrid(16));
+});
